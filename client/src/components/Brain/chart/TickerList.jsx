d6355fdcf1c6222b35ed9a5490b021d848e7c387
ryananger <nightcrawling@gmail.com>
2022-12-29 23:29:42 -0500

    Further tickerList styling.

--- client/src/components/Brain/chart/TickerList.jsx
+++ client/src/components/Brain/chart/TickerList.jsx
@@ -5,31 +5,34 @@ const TickerList = function({tickers}) {
   var render = function() {
     var rendered = [];
 
-    rendered.push(
-      <div key='listLabels' className='ticker h'>
-        <div className='tickerKey'>ticker</div>
-        <div className='tickerKey'>date</div>
-        <div className='tickerKey'>open</div>
-        <div className='tickerKey'>close</div>
-        <div className='tickerKey'>high</div>
-        <div className='tickerKey'>low</div>
-        <div className='tickerKey'>volume</div>
-      </div>
-    );
-
+    var i = 0;
     for (var key in tickers) {
       var mostRecent = tickers[key][0];
+      var tag = '';
+
+      i++;
+      if (i % 2 === 0) {
+        tag = 'alt2';
+      }
 
       rendered.push(
-        <div key={mostRecent.ticker} className='ticker h'>
-          <div className='tickerKey'><b>{mostRecent.ticker}</b></div>
-
-          <div className='tickerKey'>{ch.smallDate(mostRecent.date)}</div>
-          <div className='tickerKey'>{mostRecent.open}</div>
-          <div className='tickerKey'>{mostRecent.close}</div>
-          <div className='tickerKey'>{mostRecent.high}</div>
-          <div className='tickerKey'>{mostRecent.low}</div>
-          <div className='tickerKey'>{mostRecent.volume}</div>
+        <div key={mostRecent.ticker} className={'ticker v ' + tag}>
+          <div className='tickerHead h'>
+            <div className='tickerKey h'>
+              <b>{mostRecent.ticker}</b>
+              {ch.smallDate(mostRecent.date)}
+            </div>
+            <div className='smallButton'/>
+          </div>
+          <div className='tickerRow h'>
+            <div className='tickerKey h'><b>Open:</b>  {mostRecent.open}</div>
+            <div className='tickerKey h'><b>High:</b> {mostRecent.high}</div>
+          </div>
+          <div className='tickerRow h'>
+            <div className='tickerKey h'><b>Close:</b> {mostRecent.close}</div>
+            <div className='tickerKey h'><b>Low:</b>  {mostRecent.low}</div>
+          </div>
+          <div className='tickerVol h'><b>Volume:</b> {mostRecent.volume}</div>
         </div>
       );
     }
@@ -38,8 +41,15 @@ const TickerList = function({tickers}) {
   };
 
   return (
-    <div className='tickerList'>
-      {render()}
+    <div className='listContainer h'>
+      <div className='watchList'>
+        This will contain some set of lists that expands to view tickers in each list.
+        <br/><br/>
+        The brain will also have access to this interface.
+      </div>
+      <div className='tickerList'>
+        {render()}
+      </div>
     </div>
   )
 }
